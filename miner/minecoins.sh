daebc4f08744ca995ac730df93a581a943b12fc2
BargainBinBastard <robbieroiy@gmail.com>
2025-09-03 16:43:42 -0400

    I realized I was doing this wrong (wasn't generating nonces) so here is a corrected version of minecoins.sh and coins.txt

--- miner/minecoins.sh
+++ miner/minecoins.sh
@@ -4,12 +4,18 @@
 touch coins.txt
 
 thing=" "
-
+counter=0
 for word in $(cat dictionary.txt); do
-	thing=$(printf "%s" "$word" | sha256sum | awk '{print$1}')
+	nonce=$((10000 + RANDOM % 90000))
+	thing=$(printf "%s%s" "$nonce" "$word" | sha256sum | awk '{print$1}')
 	if [[ ${thing:0:3} == "000" ]]; then
 		
-		echo "$thing  - $word" >> coins.txt
+		echo "$thing  - $nonce$word" >> coins.txt
+		echo "$thing  - $nonce$word"
+		counter=$((counter + 1))
+	fi
+	if [[ $counter -eq 3 ]]; then
+	break
 	fi
 	
 done
